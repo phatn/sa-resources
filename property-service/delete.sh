ab6ffb083ce796b5cd0a169275d384e7b66cca39
Thiep Ha - Henry <thiep.ha@miu.edu>
2022-10-25 14:45:47 -0500

    Use common config map

--- property-service/delete.sh
+++ property-service/delete.sh
@@ -2,4 +2,3 @@
 
 kubectl delete -f PropertyService.yml
 kubectl delete -f PropertyDeployment.yml
-kubectl delete -f PropertyConfig.yml
--- property-service/deploy.sh
+++ property-service/deploy.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 
-kubectl apply -f PropertyConfig.yml
 kubectl apply -f PropertyDeployment.yml
 kubectl apply -f PropertyService.yml
